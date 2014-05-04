da76c8d65a3a057753cc1a1f8e496076390fb47b
Fukuball Lin <fukuball@gmail.com>
2014-05-04 08:44:10 +0800

    新增首頁

---
+++ app/views/partials/user-avatar-card.blade.php
@@ -0,0 +1,6 @@
+<div class="col-xs-18 col-sm-6 col-md-3 margin-top-3">
+    <center>
+        <img src="<?php echo $user_obj->avatar_url; ?>" width="140" height="140" class="img-circle">
+        <h3><?php echo $user_obj->name; ?></h3>
+    </center>
+</div>
