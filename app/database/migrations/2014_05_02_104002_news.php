b710d016e9d0851c55fbb8c085f969e51f9c2b2c
Ruoshi.Ling <fntsrlike@gmail.com>
2014-05-30 00:52:05 +0800

    Fixed issue #2. solved the problem of migrate cause of class name and file name of schema is not match.

--- app/database/migrations/2014_05_02_104002_news.php
+++ app/database/migrations/2014_05_02_104002_news.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class NewsCreate extends Migration {
+class News extends Migration {
 
 	/**
 	 * Run the migrations.
