79dc7cef51c1aa217da4098b74c83be3c54e4367
Fukuball Lin <fukuball@gmail.com>
2014-09-02 20:04:22 +0800

    debugging

--- app/views/me/issue-score-page.blade.php
+++ app/views/me/issue-score-page.blade.php
@@ -1,58 +1,5 @@
 @extends('layout')
 
 @section('content')
-    <?php
-    if (Auth::check()) {
-
-        $login_user_obj = Auth::user();
-    ?>
-    <section>
-        <h2>你的議題立場</h2>
-        <section class="issue-list-block row">
-
-            <div class="list-group">
-                <?php
-
-                $issue_list = DB::table('issues')
-                                    ->join('user_issue_score_records', 'issues.id', '=', 'user_issue_score_records.issue_id')
-                                    ->where('user_issue_score_records.user_id', $login_user_obj->id)
-                                    ->orderBy('issues.id', 'desc')
-                                    ->paginate(4);
-
-                if (!empty($issue_list)) {
-
-                    foreach ($issue_list as $issue_obj) {
-
-                        echo View::make(
-                                    'partials.issue-card',
-                                    array(
-                                        'issue_obj' => $issue_obj
-                                    )
-                                );
-                    }
-
-                }
-
-                ?>
-            </div>
-            <div class="text-center">
-                <?php
-
-                if (!empty($issue_list)) {
-
-                    echo $issue_list->links();
-
-                }
-
-                ?>
-            </div>
-        </section>
-    </section>
-    <?php
-    } else {
-
-        echo View::make('partials.login-link-alert');
-
-    }
-    ?>
+    測試
 @stop
