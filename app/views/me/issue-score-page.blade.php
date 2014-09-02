7d109cccd9a899da8eeb875c54e4f5d6beae2f69
Fukuball Lin <fukuball@gmail.com>
2014-09-02 19:57:39 +0800

    debugging

--- app/views/me/issue-score-page.blade.php
+++ app/views/me/issue-score-page.blade.php
@@ -16,23 +16,35 @@
                 $issue_list = DB::table('issues')
                                     ->join('user_issue_score_records', 'issues.id', '=', 'user_issue_score_records.issue_id')
                                     ->where('user_issue_score_records.user_id', $login_user_obj->id)
-                                    ->orderBy('issues.id', 'desc');
+                                    ->orderBy('issues.id', 'desc')
                                     ->paginate(4);
 
-                foreach ($issue_list as $issue_obj) {
+                if (!empty($issue_list)) {
 
-                    echo View::make(
-                                'partials.issue-card',
-                                array(
-                                    'issue_obj' => $issue_obj
-                                )
-                            );
+                    foreach ($issue_list as $issue_obj) {
+
+                        echo View::make(
+                                    'partials.issue-card',
+                                    array(
+                                        'issue_obj' => $issue_obj
+                                    )
+                                );
+                    }
+
                 }
 
                 ?>
             </div>
             <div class="text-center">
-                <?php echo $issue_list->links(); ?>
+                <?php
+
+                if (!empty($issue_list)) {
+
+                    echo $issue_list->links();
+
+                }
+
+                ?>
             </div>
         </section>
     </section>
